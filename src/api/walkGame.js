d15ef5620c7e22cd68a3b91c3a0a0598fa1e9a26
sego <antoniosegoviaruiz@gmail.com>
2023-12-04 16:26:53 +0100

    format

--- src/api/walkGame.js
+++ src/api/walkGame.js
@@ -1,25 +1,25 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 // start game
 const gameSize = {
-  "width": 10,
-  "height": 10
-}
+  width: 30,
+  height: 30,
+};
 
 // create new player
 const player = {
-  "username": "TheBestPlayer"
-}
+  username: 'TheBestPlayer',
+};
 
 // move player
 const playerPosition = [
   {
-    "operationType": 0,
-    "path": "/Position",
-    "op": "replace",
-    "from": "string",
-    "value": { "Row": 1, "Column": 1 }
-  }
-]
+    operationType: 0,
+    path: '/Position',
+    op: 'replace',
+    from: 'string',
+    value: { Row: 1, Column: 1 },
+  },
+];
 
 export const api = createApi({
   reducerPath: 'walkingMan',
@@ -32,43 +32,43 @@ export const api = createApi({
         url: '/game',
         method: 'POST',
         body: gameSize,
-      })
+      }),
     }),
     addPlayer: builder.mutation({
       query: () => ({
         url: '/player',
         method: 'POST',
         body: player,
-      })
+      }),
     }),
     movePlayer: builder.mutation({
       query: () => ({
         url: '/player/TheBestPlayer',
         method: 'PATCH',
         body: playerPosition,
-      })
+      }),
     }),
     getPlayerInfo: builder.query({
-        query: () => ({
-          url: '/player?username=TheBestPlayer',
-          method: 'GET',
-        })
+      query: () => ({
+        url: '/player?username=TheBestPlayer',
+        method: 'GET',
+      }),
     }),
     deletePlayer: builder.mutation({
-        query: () => ({
-          url: '/player',
-          method: 'DELETE',
-          body: player,
-        })
+      query: () => ({
+        url: '/player',
+        method: 'DELETE',
+        body: player,
+      }),
     }),
     getGame: builder.query({
-        query: () => ({
-          url: '/game',
-          method: 'GET',
-        })
+      query: () => ({
+        url: '/game',
+        method: 'GET',
+      }),
     }),
-  })
-})
+  }),
+});
 
 export const {
   useStartGameMutation,
@@ -78,5 +78,3 @@ export const {
   useDeletePlayerMutation,
   useGetGameQuery,
 } = api;
-
-
