41836e2b3120ed84ccc6e5e10efcca95a9dbf98f
sego <antoniosegoviaruiz@gmail.com>
2023-12-04 14:41:49 +0100

    linter -> default to airbnb

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
  },
   extends: [
     'eslint:recommended',
+    'airbnb',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
